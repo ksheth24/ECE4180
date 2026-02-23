fb2903f4956b3ed74a9a8aa2802f8456b7370c9a
Satvik Malneedi <56760653+satvikmalneedi@users.noreply.github.com>
2026-02-23 15:15:02 -0500

    Changes to part 8

--- Lab 2/Part8.ino
+++ Lab 2/Part8.ino
@@ -1,22 +1,37 @@
 #include <Wire.h>
 #include <ICM_20948.h>
 #include <Adafruit_MPR121.h>
+#include <Goldelox_Serial_4DLib.h>
+#include <Goldelox_Const4D.h>
+
+#define DisplaySerial Serial1
+
+
 
 #define INTR 20
 #define AD0_VAL 1
 ICM_20948_I2C myICM;
 
 Adafruit_MPR121 cap = Adafruit_MPR121();
+Goldelox_Serial_4DLib Display(&DisplaySerial);
+
 #ifndef _BV
 #define _BV(bit) (1 << (bit))
 #endif
 
+int j = 0;
+String s = "";
+String sNew = "";
+bool correct = false;
+String answer = "147";
+String ansNew = "LeftRightFor";
 
 void setup() {
   Serial.begin(115200);
 
   Wire.begin(23, 22);
   Wire.setClock(400000);
+
   myICM.begin(Wire, AD0_VAL);
   Serial.println("ICM Configed");
 
@@ -30,6 +45,10 @@ void setup() {
 
   Serial.println("MPR121 ready");
   pinMode(INTR, OUTPUT);
+  
+  DisplaySerial.begin(9600, SERIAL_8N1, RX, TX);
+  Display.TimeLimit4D = 500;
+  Display.gfx_Cls();
 }
 
 void loop() {
@@ -37,6 +56,14 @@ void loop() {
   int touched = cap.touched();
   for (int i = 0; i < 12; i++) {
     if (touched & _BV(i)) {
+      if (i == 10) {
+        if (s.equals(answer) && sNew.equals(ansNew))
+      }
+      if (i == 11) {
+        s = s.substring(0, s.length() - 1);
+      } else {
+        s += i;
+      } 
       Serial.print(" Key ");
       Serial.print(i);
       Serial.println(" touched");
@@ -46,12 +73,23 @@ void loop() {
     Serial.println("Data Recieved");
     myICM.getAGMT();
     if (myICM.accX() > 400) {
-      
+      sNew += "Left";
     } else if (myICM.accX() < -400) {
-      
+      sNew += "Right";
     } else if (myICM.accY() > 400) {
-      
+      sNew += "For";
+    } else if (myICM.accY() < -400) {
+      sNew += "Back";
+    } else if (myICM.accZ() - 1000 < -200) {
+      sNew += "Down";
+    } else if (myICM.accZ() - 1000 > 200) {
+      sNew += "Up";
     }
   }
+  Display.gfx_Cls();
+Display.gfx_MoveTo(10, 50);   // x=10px from left, y=50px from top
+Display.print(s);
+Display.gfx_MoveTo(10, 70);   // x=10px from left, y=50px from top
+Display.print(sNew);
   delay(100);
 }
