f82fd5d1f0fa2770c4fdc4af51dc39e93bc69b10
Satvik Malneedi <56760653+satvikmalneedi@users.noreply.github.com>
2026-04-01 18:29:21 -0400

    Implement AI tasks and NeoPixel initialization
    Added AI/ML functionality and integrated NeoPixel support.

--- Lab4/Part4.ino
+++ Lab4/Part4.ino
@@ -10,6 +10,13 @@ TaskHandle_t Task2;
 TaskHandle_t Task3;
 
 
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 8        // or whatever your board uses
+#define NUM_PIXELS 1     // usually 1 for these labs
+
+Adafruit_NeoPixel pixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 // --- Declare Semaphore and Mutex Stuff ---
 
 
@@ -32,13 +39,77 @@ NimBLECharacteristic* pCharacteristic = nullptr;
 uint8_t* code = 0;
 
 
+uint8_t possibilities[ARRAY_SIZE][VECTOR_SIZE];
+bool active[ARRAY_SIZE];
+
+int currentGuessIdx;
+uint8_t turns;
+bool gameRunning;
+
+float classifierConfidence[3] = {0, 0, 0}; // [descending, low_numbers, two_pairs] - Model is in different order than style selection
+uint8_t dealerStyle; // Randomly generated dealer style
+uint8_t currentDealerStyle; // What the ML will recognize as the dealer's style
+
 // -------------------------------------------------------------------------
 // CORE 1: AI & ML TASKS
 // -------------------------------------------------------------------------
 
 // Task A: Constant ML Inference (Triggered by data)
-void mlInferenceTask(void *pvParameters) {
-    
+void populateArray(uint8_t (*array)[VECTOR_SIZE], uint32_t size) {
+  uint8_t counters[VECTOR_SIZE] = {0};
+
+  for (uint32_t i = 0; i < size; i++) {
+    active[i] = true;
+    // Copy current vector
+    memcpy(array[i], counters, VECTOR_SIZE);
+
+    counters[0]++;
+    for (uint8_t k = 0; k < VECTOR_SIZE - 1; k++) {
+      if (counters[k] >= BASE) {
+        counters[k] = 0;
+        counters[k + 1]++;
+      }
+    }
+  }
+}f    int uniqueCount = 0;
+    int isDescending = code[0] >= code[1] && code[1] >= code[2] && code[2] >= code[3];
+    int maxV = 0;
+    bool unique = false;
+    for (int i = 0; i < 4; i++) {
+      unique = true;
+      if (code[i] > maxV) {
+        maxV = code[i];
+      }
+      for (int j = i - 1; j >= 0; j--) {
+        if (code[j] == code[i]) {
+          unique = false;
+        }
+      }
+      if (unique) {
+        uniqueCount++;
+      }
+    }
+
+    // TODO: Populate a float array of features with the following order:
+    // code[0], code[1], code[2], code[3], uniqueCount, isDescending, maxV
+    float features[7];
+    features[0] = code[0];
+    features[1] = code[1];
+    features[2] = code[2];
+    features[3] = code[3];
+    features[4] = uniqueCount;
+    features[5] = isDescending;
+    features[6] = maxV;
+
+    // Wrap the features for the EON compiler
+    signal_t signal;
+    numpy::signal_from_buffer(features, 7, &signal);
+
+    // Run inference
+    ei_impulse_result_t result = { 0 };
+    EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false);
+
+    if (res != EI_IMPULSE_OK) return;
 }
 
 // Button ISR (Required)
@@ -48,8 +119,183 @@ void IRAM_ATTR handleButtonPress() {
 
 // Task B: AI Guess Making (Triggered by Button)
 void aiGuessTask(void *pvParameters) {
-    
+  populateArray
+
+  prePrune(currentDealerStyle);
+
+
+  turns = 0;
+
+  while (turns < 10) {
+      int guessIdx = getBestGuess();
+      uint8_t* guess = possibilities[guessIdx];
+
+      uint8_t result[2];
+
+      
+
+      Serial.printf("Guess: %d %d %d %d\n",
+          guess[0], guess[1], guess[2], guess[3]);
+      Serial.printf("Right: %d %d %d %d\n",
+          actual[0], actual[1], actual[2], actual[3]);
+      Serial.printf("Results: %d | %d\n\n",
+          result[0], result[1]);
+
+      // solved
+      if (result[0] == 4) {
+          Serial.printf("I solved it in %d turns! Somethin' light. :)\n", turns);
+          delay(1000);
+          break;
+      }
+
+      // eliminate impossible codes
+      prune(guess, result);
+
+      turns++;
+    }
 }
+
+void populateArray(uint8_t (*array)[VECTOR_SIZE], uint32_t size) {
+  uint8_t counters[VECTOR_SIZE] = {0};
+
+  for (uint32_t i = 0; i < size; i++) {
+    active[i] = true;
+    // Copy current vector
+    memcpy(array[i], counters, VECTOR_SIZE);
+
+    counters[0]++;
+    for (uint8_t k = 0; k < VECTOR_SIZE - 1; k++) {
+      if (counters[k] >= BASE) {
+        counters[k] = 0;
+        counters[k + 1]++;
+      }
+    }
+  }
+}
+
+void populateArray(uint8_t (*array)[VECTOR_SIZE], uint32_t size) {
+  uint8_t counters[VECTOR_SIZE] = {0};
+
+  for (uint32_t i = 0; i < size; i++) {
+    active[i] = true;
+    // Copy current vector
+    memcpy(array[i], counters, VECTOR_SIZE);
+
+    counters[0]++;
+    for (uint8_t k = 0; k < VECTOR_SIZE - 1; k++) {
+      if (counters[k] >= BASE) {
+        counters[k] = 0;
+        counters[k + 1]++;
+      }
+    }
+  }
+}
+
+int getBestGuess() {
+    int bestGuessIdx = -1;
+    int minMaxRemaining = 2000;
+
+    Serial.println("Calculating best guess...");
+    uint32_t start = millis();
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        if (i == 0) {
+            int activeCount = 0;
+            for(int k=0; k<ARRAY_SIZE; k++) if(active[k]) activeCount++;
+            Serial.printf("Active codes remaining: %d\n", activeCount);
+        }
+        // Heartbeat print every 200 iterations so you know it hasn't crashed
+        if (i % 200 == 0) Serial.print("."); 
+
+        // KNUTH'S RULE: We can guess ANY code (even inactive ones)
+        // to gain maximum information. Equivalent to in Wordle when you have something
+        // like _ATCH so you guess PLUMB to narrow it down even if it doesn't work
+        
+        int scoreCount[5][5] = {0}; 
+        for (int j = 0; j < ARRAY_SIZE; j++) {
+            if (!active[j]) continue;
+            
+            uint8_t check[2];
+            dealer.compare(check, possibilities[i], possibilities[j]);
+            scoreCount[check[0]][check[1]]++;
+        }
+
+        int maxForThisGuess = 0;
+        for(int b=0; b<=4; b++) {
+            for(int w=0; w<=4; w++) {
+                if(scoreCount[b][w] > maxForThisGuess) 
+                    maxForThisGuess = scoreCount[b][w];
+            }
+        }
+
+        if (maxForThisGuess < minMaxRemaining) {
+            minMaxRemaining = maxForThisGuess;
+            bestGuessIdx = i;
+        } // If it's a tie, prefer a code that could actually be the secret
+        else if (maxForThisGuess == minMaxRemaining && active[i] && !active[bestGuessIdx]) {
+            bestGuessIdx = i;
+        }
+    }
+    
+    Serial.printf("\nDone! Took %d ms. Best Index: %d\n", (millis() - start), bestGuessIdx);
+    return bestGuessIdx;
+}
+
+void prePrune(uint8_t predictedStyle) {
+    // TODO: Prune the array of possibilities to remove anything that doesn't match your recognized style
+    // Similar to prune, but we're now removing things that don't match the given style
+    // Similar logic as classifying the dealer's style from last part
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+      if (!active[i]) {
+        continue;
+      }
+      uint8_t* code = possibilities[i];
+      int uniqueCount = 0;
+      int isDescending = code[0] >= code[1] && code[1] >= code[2] && code[2] >= code[3];
+      int maxV = 0;
+      bool unique = false;
+      for (int k = 0; k < 4; k++) {
+        unique = true;
+        if (code[k] > maxV) {
+          maxV = code[k];
+        }
+        for (int j = k - 1; j >= 0; j--) {
+          if (code[j] == code[k]) {
+            unique = false;
+          }
+        }
+        if (unique) {
+          uniqueCount++;
+        }
+      }
+      if (predictedStyle == 0) {         
+          if (!isDescending) active[i] = false;
+      }
+      else if (predictedStyle == 1) {    
+          if (maxV > 2) active[i] = false;
+      }
+      else if (predictedStyle == 2) {    
+          if (uniqueCount != 2) active[i] = false;
+      }
+    }
+
+}
+
+void prune(uint8_t* lastGuess, uint8_t* results) {
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        if (!active[i]) continue; // Already pruned
+
+        // "If possibilities[i] was the secret, what feedback would it give?"
+        uint8_t reference[2];
+        dealer.compare(reference, possibilities[i], lastGuess);
+
+        // Remove possible answers that do not match
+        if (reference[0] != results[0] || reference[1] != results[1]) {
+            active[i] = false; 
+        } 
+    }
+}
+
 
 // -------------------------------------------------------------------------
 // CORE 0: BLE & WEB TASK
@@ -178,7 +424,7 @@ void setup() {
     // TODO: Initialize your button with an ISR (you will need to configure an ISR with this RTOS)
     pinMode(BUTTON_PIN, INPUT);    // external hardware debounce
 
-    attachInterrupt(BUTTON_PIN, handleButtonPress, RISING);
+    // attachInterrupt(BUTTON_PIN, handleButtonPress, RISING);
     
     // TODO: Pin Tasks to Cores
     // Core 1: AI and ML (Higher priority for pruning)
@@ -212,6 +458,8 @@ void setup() {
       0);
     delay(500);
 
+  pixel.begin();
+  pixel.show();
 }
 
 void loop() {
